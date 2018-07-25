4fc85fc8f06e11c00b02dac1fc1a9c225b4987e6
Tomas <oyarzun.tomas@gmail.com>
2018-07-25 01:51:42 -0400

    ADD: Method student_avg

--- prueba.rb
+++ prueba.rb
@@ -3,6 +3,25 @@ def cls
 	system "clear"
 end
 
+def student_avg
+	#Reading archive.csv
+	students = nil
+	File.open('archive.csv', 'r'){ |info| students = info.readlines.map(&:chomp) }
+
+	students.each do |student|
+	 	a = student.split(', ')[0] #For names
+	 	b = student.split(', ')[1..5] #For grades
+	 	c = b.inject(0){ |acum, i| acum + i.to_f } / b.length #For grades
+
+	 	#Creating student_avg.csv
+	 	File.open('student_avg.csv', 'a'){ |file| file.puts "#{a} tiene un promedio de: #{c}"}
+	end
+	#Reading student_avg.csv
+	answ = nil
+	File.open('student_avg.csv', 'r'){ |result| answ = result.readlines }
+	puts answ
+end
+
 def main_menu
 	option = 0
 	while option != 4
@@ -27,5 +46,5 @@ def main_menu
 	end
 end
 
-
-
+#Main
+main_menu
